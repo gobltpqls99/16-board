494d3d777822533ca7967e7af57cbc8b483ec502
gobltpqls99 <gobltpqls99@naver.com>
2021-01-27 14:30:13 +0900

    ..

---
+++ database.sql
@@ -0,0 +1,40 @@
+-- --------------------------------------------------------
+-- 호스트:                          127.0.0.1
+-- 서버 버전:                        8.0.23 - MySQL Community Server - GPL
+-- 서버 OS:                        Win64
+-- HeidiSQL 버전:                  11.2.0.6213
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- sb 데이터베이스 구조 내보내기
+CREATE DATABASE IF NOT EXISTS `sb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sb`;
+
+-- 테이블 sb.board 구조 내보내기
+CREATE TABLE IF NOT EXISTS `board` (
+  `id` int DEFAULT NULL,
+  `title` varchar(50) DEFAULT NULL,
+  `content` text,
+  `writer` varchar(50) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `orifile` varchar(50) DEFAULT NULL,
+  `savefile` varchar(50) DEFAULT NULL,
+  `readnum` int DEFAULT NULL,
+  `uid` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- 테이블 데이터 sb.board:~0 rows (대략적) 내보내기
+/*!40000 ALTER TABLE `board` DISABLE KEYS */;
+/*!40000 ALTER TABLE `board` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- routes/board-route.js
+++ routes/board-route.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../modules/mysql-pool');
+const { err } = require('../modules/util');
 const pugs = { 
 	css: 'board', 
 	js: 'board', 
@@ -12,7 +14,21 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/create', (req, res, next) => {
-	res.render('board/create', pugs);
+	const pug = { ...pugs, tinyKey: process.env.TINY_KEY }
+	res.render('board/create', pug);
+});
+
+router.post('/save', async (req, res) => {
+	try {
+		const { title, content, writer } = req.body;
+		const sql = 'INSERT INTO board SET title=?, content=?, writer=?';
+		const value = [title, content, writer];
+		const r = await pool.query(sql, value);
+		res.json(r[0]);
+	}
+	catch(e) {
+		next(err(e));
+	}
 });
 
 module.exports = router;
