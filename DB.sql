c75d13c6fdbd6988531aaa5f07ba0afc42c773b3
gobltpqls99 <gobltpqls99@naver.com>
2021-03-04 09:53:52 +0900

    완성

--- DB.sql
+++ DB.sql
@@ -14,50 +14,50 @@
 
 
 -- sb 데이터베이스 구조 내보내기
-CREATE DATABASE IF NOT EXISTS `sb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE IF NOT EXISTS `sb` /*!40100 DEFAULT CHARACTER SET utf8mb4  */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `sb`;
 
 -- 테이블 sb.auth 구조 내보내기
 CREATE TABLE IF NOT EXISTS `auth` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
   `userid` varchar(50) NOT NULL,
-  `userpw` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `userpw` varchar(255) CHARACTER SET utf8mb4  NOT NULL,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `grade` tinyint unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=;       
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 sb.board 구조 내보내기
 CREATE TABLE IF NOT EXISTS `board` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `content` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `writer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET   NOT NULL,
+  `content` text CHARACTER SET  ,      
+  `writer` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL,
   `created` datetime DEFAULT NULL,
-  `orifile` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `savefile` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `orifile` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL,
+  `savefile` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL,
   `readnum` int unsigned NOT NULL DEFAULT '0',
   `uid` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 ;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 sb.board_ip 구조 내보내기
 CREATE TABLE IF NOT EXISTS `board_ip` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `ip` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `ip` varchar(50) CHARACTER SET utf8mb4  NOT NULL,
   `wdate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `bid` int unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `bid` (`bid`),
   CONSTRAINT `FK_board-ip_board` FOREIGN KEY (`bid`) REFERENCES `board` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 ;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
@@ -68,22 +68,22 @@ CREATE TABLE IF NOT EXISTS `books` (
   `writer` varchar(255) DEFAULT NULL,
   `wdate` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 ;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 sb.city 구조 내보내기
 CREATE TABLE IF NOT EXISTS `city` (
   `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '순번',
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '도시명',
-  `country` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '국가명',
-  `summary` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '도시설명',
+  `name` varchar(255) CHARACTER SET utf8mb4  NOT NULL COMMENT '도시명',
+  `country` varchar(255) CHARACTER SET utf8mb4  DEFAULT NULL COMMENT '국가명',
+  `summary` text CHARACTER SET utf8mb4  COMMENT '도시설명',
   `lat` float unsigned DEFAULT NULL COMMENT '위도',
   `lon` float unsigned DEFAULT NULL COMMENT '경도',
   `population` int unsigned DEFAULT NULL COMMENT '인구수',
   `sdate` datetime DEFAULT NULL COMMENT '도시설립일',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 ;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
@@ -99,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `gallery` (
   PRIMARY KEY (`id`),
   KEY `uid` (`uid`),
   CONSTRAINT `FK_gallery_auth` FOREIGN KEY (`uid`) REFERENCES `auth` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 ;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
@@ -113,20 +113,20 @@ CREATE TABLE IF NOT EXISTS `gallery_file` (
   PRIMARY KEY (`id`),
   KEY `fid` (`fid`) USING BTREE,
   CONSTRAINT `FK_gallery_file_gallery` FOREIGN KEY (`fid`) REFERENCES `gallery` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4  ROW_FORMAT=DYNAMIC;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
 -- 테이블 sb.gallery_ip 구조 내보내기
 CREATE TABLE IF NOT EXISTS `gallery_ip` (
   `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `ip` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `ip` varchar(50) CHARACTER SET utf8mb4  NOT NULL DEFAULT '',
   `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `bid` int unsigned DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `fid` (`bid`),
   CONSTRAINT `FK_gallery_ip_gallery` FOREIGN KEY (`bid`) REFERENCES `gallery` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4  ROW_FORMAT=DYNAMIC;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
@@ -136,7 +136,7 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   `expires` int unsigned NOT NULL,
   `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
 -- 내보낼 데이터가 선택되어 있지 않습니다.
 
